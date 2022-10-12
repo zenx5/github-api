3d3f6700d6387f2502d7126bab92c46e4d7ff0d6
Octavio Martinez <omartinez1618@gmail.com>
2022-10-12 18:37:35 -0400

    get data cool

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 import * as dotenv from "dotenv"
-import * as express from "express"
+dotenv.config()
+import express from "express"
 import axios from "axios"
 
-dotenv.config()
+
 
 const app = express()
 const port = 3000
 
 
-app.get("get", (req, res) => {
-    
+app.get("/", async (req, res) => {
+    const result = []
+    for(let indexIterator=0; indexIterator < 10; indexIterator++ ){
+        const { data, status} = await axios.get(String(process.env.GITHUB_RAW_URL).replace('{index}', indexIterator))
+        result.push({
+            url: process.env.GITHUB_RAW_URL.replace('{index}', indexIterator),
+            content: data
+        })
+    }
+    let echo = ''
+    result.forEach( (item,index) => {
+        const regExp = /dificultad: [FÁCIL|MEDIA|DIFÍCIL]{1,}/gi
+        echo += "<div><a href='"+item.url+"'>Reto "+ index +"</a><br>"
+        echo += "Difcultad : "+item.content.match(/dificultad: [FÁCIL|MEDIA|DIFÍCIL]{1,}/gi)[0].split(': ')[1]
+        echo += "</div>"
+    })
+    res.send(echo)
+})
+
+app.get("/test", async (req, res) => {
+
+    res.send("url: " + process.env.GITHUB_RAW_URL)
 })
 
 app.listen(port, ()=> console.log("hola, i'm running"))
