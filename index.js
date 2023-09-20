acfd5abd0f5dcce34959748157b0e1dd5b1083c6
Octavio Martinez <omartinez1618@gmail.com>
2023-09-20 17:43:11 -0400

    search dep

--- index.js
+++ index.js
@@ -8,24 +8,80 @@ import axios from "axios"
 const app = express()
 const port = 3000
 
-
-app.get("/", async (req, res) => {
+const getLinks = (start, end, callback) => async (req, res) => {
     const result = []
-    for(let indexIterator=0; indexIterator < 10; indexIterator++ ){
+    for(let indexIterator=start; indexIterator < end; indexIterator++ ){
         const { data, status} = await axios.get(String(process.env.GITHUB_RAW_URL).replace('{index}', indexIterator))
         result.push({
             url: process.env.GITHUB_RAW_URL.replace('{index}', indexIterator),
             content: data
         })
     }
+    res.send( callback( result ) )
+}
+
+app.get("/", getLinks(0, 10, result => {
     let echo = ''
     result.forEach( (item,index) => {
         const regExp = /dificultad: [FÁCIL|MEDIA|DIFÍCIL]{1,}/gi
         echo += "<div><a href='"+item.url+"'>Reto "+ index +"</a><br>"
-        echo += "Difcultad : "+item.content.match(/dificultad: [FÁCIL|MEDIA|DIFÍCIL]{1,}/gi)[0].split(': ')[1]
+        echo += "Difcultad : "+item.content.match(regExp)[0].split(': ')[1]
         echo += "</div>"
     })
-    res.send(echo)
+    return echo
+}))
+
+app.get("/json", getLinks(0,10, result => result ))
+app.get("/json/:id", (req, res)=>getLinks(
+    parseInt( req.params.id ),
+    parseInt( req.params.id ) + 1,
+    result => result )(req, res)
+)
+
+app.get("/search/:user/:dep", async (req, res) => {
+    const { user, dep } = req.params
+    const { data, status} = await axios.get(`https://api.github.com/users/${user}/repos`)
+    const packages = data.map( ({name, default_branch, html_url}) => ({
+        package: `https://raw.githubusercontent.com/${user}/${name}/${default_branch}/package.json`,
+        html_url
+    }) )
+    let result = []
+    let errors = []
+    for( let index = 0; index<packages.length; index++ ) {
+        const fileuri = packages[ index ].package
+        try{
+            console.log("url", fileuri)
+            const { data:packageContent } = await axios.get(fileuri)
+            const keyDependencies = [
+                ...Object.keys( packageContent?.devDependencies || {} ),
+                ...Object.keys( packageContent?.dependencies || {} )
+            ]
+            console.log( keyDependencies )
+            if( keyDependencies.includes(dep) ){
+                result.push( packages[ index ].html_url )
+            }else {
+                throw new Error("dependencie not found")
+            }
+        }catch( error ) {
+            errors.push( { name: fileuri, error: error.message } )
+        }
+    }
+
+    res.send( {
+        data: result,
+        errors
+    } )
+})
+
+app.get("/:user/repos", async(req, res) => {
+    const { user } = req.params
+    const { data, status} = await axios.get(`https://api.github.com/users/${user}/repos`)
+    res.send( data )
+})
+
+app.get("/file", async(req, res) => {
+    const { data } = await axios.get("https://raw.githubusercontent.com/zenx5/adrian-pasantia/main/package.json")
+    res.send( data )
 })
 
 app.get("/test", async (req, res) => {
